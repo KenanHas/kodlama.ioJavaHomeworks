60e597f3a209f936db360bc7e233b907177108eb
haske <haske@DESKTOP-184BDR2>
2022-09-20 10:33:39 +0300

    try

---
+++ kodlama.io/src/Week1/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package Week1;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println(i);
+			i+=2;
+			
+		}while(i<10);
+		System.out.println("This is out of the loop.");
+		
+		
+	}
+
+}
