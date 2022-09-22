c316ec5cbe608edf13c660232d3749923b26b187
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-22 18:53:17 +0300

    Create Classes.java

---
+++ kodlama.io/src/Week2/Classes.java
@@ -0,0 +1,14 @@
+package Week2;
+
+public class Classes {
+
+	public static void main(String[] args) {
+		CustomerManager firstCustomer = new CustomerManager();
+
+		firstCustomer.Add();
+		firstCustomer.Remove();
+		firstCustomer.Update();
+
+	}
+
+}
