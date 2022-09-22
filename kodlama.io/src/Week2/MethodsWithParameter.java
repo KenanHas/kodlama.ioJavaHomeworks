ca1928ac18d66939385afc7270168ebe1bea04b6
haske <haske@DESKTOP-184BDR2>
2022-09-22 16:15:18 +0300

    25. Lesson

---
+++ kodlama.io/src/Week2/MethodsWithParameter.java
@@ -0,0 +1,15 @@
+package Week2;
+
+public class MethodsWithParameter {
+
+	public static void main(String[] args) {
+		
+		System.out.println(summation(13,5));
+	}
+	public static int summation(int numberOne,int numberTwo)
+	{
+		int total = numberOne+numberTwo;
+		return total;
+	}
+	
+}
