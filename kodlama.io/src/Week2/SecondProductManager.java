a08c96ea00057bdf37b0a3dd4af6e0e9bc8b4a43
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-23 16:33:24 +0300

    Create SecondProductManager.java

---
+++ kodlama.io/src/Week2/SecondProductManager.java
@@ -0,0 +1,8 @@
+package Week2;
+
+//LESSON 33
+public class SecondProductManager {
+	public void Add(SecondProduct secondProduct) {
+		System.out.println("Product added:" + " " + secondProduct.getName());
+	}
+}
