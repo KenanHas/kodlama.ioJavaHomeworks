7a8e6b418c8f77159155963217b7943292597279
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-23 21:51:44 +0300

    Lesson33

---
+++ kodlama.io/src/Week2/GetterSetter/SecondProductManager.java
@@ -0,0 +1,10 @@
+package Week2;
+
+import Week2.GetterSetter.SecondProduct;
+
+//LESSON 33
+public class SecondProductManager {
+	public void Add(SecondProduct secondProduct) {
+		System.out.println("Product added:" + " " + secondProduct.getName());
+	}
+}
