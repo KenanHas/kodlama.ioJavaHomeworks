108bb8fcae6cc4c314af60d673ad81b74572666b
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-23 19:40:16 +0300

    Create MethodOverloading.java

---
+++ kodlama.io/src/Week2/MethodOverloading.java
@@ -0,0 +1,15 @@
+package Week2;
+
+//Lesson 35
+public class MethodOverloading {
+
+	public int MethodOverloading(int numberOne, int numberTwo) {
+
+		return numberOne + numberTwo;
+	}
+
+	public int MethodOverloading(int numberOne, int numberTwo, int numberThree) {
+
+		return numberOne + numberTwo + numberThree;
+	}
+}
