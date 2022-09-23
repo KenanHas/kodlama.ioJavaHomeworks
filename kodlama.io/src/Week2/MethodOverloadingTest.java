94ecd6a5da1841960ae63101a8c6155345153574
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-23 19:41:01 +0300

    Create MethodOverloadingTest.java

---
+++ kodlama.io/src/Week2/MethodOverloadingTest.java
@@ -0,0 +1,14 @@
+package Week2;
+
+//LESSON 35
+public class MethodOverloadingTest {
+
+	public static void main(String[] args) {
+		MethodOverloading object = new MethodOverloading();
+		System.out.println(object.MethodOverloading(10, 220));
+		System.out.println("Second method");
+		System.out.println(object.MethodOverloading(1, 2, 3));
+
+	}
+
+}
