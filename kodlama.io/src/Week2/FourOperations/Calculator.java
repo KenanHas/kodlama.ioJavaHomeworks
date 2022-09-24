7c9aff676e508d8e4aeed74680f3f39aca901711
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-24 12:53:15 +0300

    Lesson30

---
+++ kodlama.io/src/Week2/FourOperations/Calculator.java
@@ -0,0 +1,24 @@
+package Week2.FourOperations;
+
+public class Calculator {
+	public int Summation(int numberOne, int numberTwo) {
+
+		return numberOne + numberTwo;
+	}
+
+	public int Substraction(int numberOne, int numberTwo) {
+
+		return numberOne - numberTwo;
+	}
+
+	public int Multiplication(int numberOne, int numberTwo) {
+
+		return numberOne * numberTwo;
+	}
+
+	public double Divide(int numberOne, int numberTwo) {
+
+		return (double) numberOne / numberTwo;
+	}
+
+}
---
+++ kodlama.io/src/Week2/FourOperations/CalculatorTest.java
@@ -0,0 +1,14 @@
+package Week2.FourOperations;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		Calculator object = new Calculator();
+		System.out.println(object.Summation(12, 4));
+		System.out.println(object.Substraction(12, 4));
+		System.out.println(object.Multiplication(12, 4));
+		System.out.println(object.Divide(15, 4));
+
+	}
+
+}
