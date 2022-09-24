f02048087dfd74c768714d6582d8c63b77af2cbd
Kenan Has <98099024+KenanHas@users.noreply.github.com>
2022-09-24 12:45:14 +0300

    Delete SecondProductManager.java

--- kodlama.io/src/Week2/SecondProductManager.java
+++
@@ -1,8 +0,0 @@
-package Week2;
-
-//LESSON 33
-public class SecondProductManager {
-	public void Add(SecondProduct secondProduct) {
-		System.out.println("Product added:" + " " + secondProduct.getName());
-	}
-}
